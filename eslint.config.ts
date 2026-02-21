29e534771046ac8c7febcfd2b2689a297fa6fe3a
vincentmin <vincentmin17@gmail.com>
2026-02-21 20:40:56 +0100

    rmeove commitlint hook

--- eslint.config.ts
+++ eslint.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(
       globals: globals.node,
     },
   },
-  { ignores: ['dist/**', 'server/**', 'node_modules/**', 'scratch/**', 'agent/**', 'commitlint.config.js'] },
+  { ignores: ['dist/**', 'server/**', 'node_modules/**', 'scratch/**', 'agent/**'] },
   {
     rules: {
       '@typescript-eslint/no-unused-vars': [
