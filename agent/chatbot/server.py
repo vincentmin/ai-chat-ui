7897dce0d9715215b3f50456e4dd8282d6dee5ed
vincentmin <vincentmin17@gmail.com>
2026-02-22 19:52:03 +0100

    use small models for speedy and cheaper development

--- agent/chatbot/server.py
+++ agent/chatbot/server.py
@@ -18,9 +18,9 @@ logfire.configure(send_to_logfire='if-token-present')
 logfire.instrument_pydantic_ai()
 
 models = {
-    'Claude Sonnet 4.5': 'anthropic:claude-sonnet-4-5',
-    'GPT 5': 'openai-responses:gpt-5',
-    'Gemini 2.5 Pro': 'google-gla:gemini-2.5-pro',
+    'Claude Sonnet 4.5': 'anthropic:claude-haiku-4-5',
+    'GPT 5': 'openai-responses:gpt-5-nano',
+    'Gemini 2.5 Pro': 'google-gla:gemini-3-flash-preview',
 }
 if not os.getenv('OPENAI_API_KEY'):
     del models['GPT 5']
